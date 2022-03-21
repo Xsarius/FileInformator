7cc49cc3a79e234c407ce775f88b17ff4964e9c0
Xsarius <60043296+Xsarius@users.noreply.github.com>
2022-03-21 16:05:28 +0100

    update final

--- MyGetFilesInfo.cs
+++ MyGetFilesInfo.cs
@@ -4,9 +4,27 @@ public class MyGetFilesInfo
     {
         if (path == "" || path == null)
             return Enumerable.Empty<MyFileData>();
+        
+        List<MyFileData> all_files = new List<MyFileData>();
+        
+        string[] dir_paths = Directory.GetDirectories(path);    
 
+        foreach(string dir_path in dir_paths)
+        {
+            all_files.AddRange(GetFilesInDir(dir_path));
+        }
+
+        all_files.AddRange(GetFilesInDir(path));
+
+        return all_files.AsEnumerable();
+    }
+
+    private List<MyFileData> GetFilesInDir(string path)
+    {
         List<MyFileData> files = new List<MyFileData>();
 
+        try
+        {
             string[] paths = Directory.GetFiles(path);
 
             foreach (string temp_path in paths)
@@ -14,14 +32,21 @@ public class MyGetFilesInfo
                 FileInfo file = new FileInfo(temp_path);
                 files.Add(new MyFileData
                 {
-                Name = file.Name,
-                LastMod = file.LastWriteTime.ToString(),
-                Size = file.Length,
-                FileType = file.Extension
+                    Name = file.Name,
+                    LastMod = file.LastWriteTime.ToString(),
+                    Size = file.Length,
+                    FileType = file.Extension
                 });
 
             }
 
-            return files.AsEnumerable();
+            return files;
+        }
+        catch (Exception ex)
+        {
+           Console.WriteLine(ex.ToString());
+        }
+
+        return files;
     }
 }
